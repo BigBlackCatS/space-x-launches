0ca16763c67846a28bead82ba7a81f160a094323
Svetlana Matviienko <svitlana.matviienko@hys-enterprise.com>
2023-07-25 02:38:24 +0300

    Added a home page.
    Added a home page.

---
+++ space-x-launches/src/components/HomeComponent.tsx
@@ -0,0 +1,12 @@
+import '../App.css';
+
+export default function Home() {
+    return (
+
+        <div className="center-container">
+            <header>
+                <h1 className="home-font">Home</h1>
+            </header>
+        </div>
+    );
+}
